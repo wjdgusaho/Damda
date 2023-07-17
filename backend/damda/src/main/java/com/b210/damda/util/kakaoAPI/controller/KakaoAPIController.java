e40f66736f656c2ebe032ec22976276f23c6746a
정현모 <wjdgusaho@naver.com>
2023-07-18 08:18:29 +0900

    feat : 카카오 API 토큰 받아오기

---
+++ backend/damda/src/main/java/com/b210/damda/util/kakaoAPI/controller/KakaoAPIController.java
@@ -0,0 +1,39 @@
+package com.b210.damda.util.kakaoAPI.controller;
+
+import com.b210.damda.util.kakaoAPI.service.KakaoAPIService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+@CrossOrigin("*")
+@RestController
+@RequestMapping("/api/kakaoapi")
+@RequiredArgsConstructor
+public class KakaoAPIController {
+
+    private final KakaoAPIService kakaoAPIService;
+
+//    public String getKakaAuthUrl(HttpServletRequest request) throws Exception{
+//        System.out.println("testet");
+//        String reqUrl = "https://kauth.kakao.com/oauth/authorize"
+//                + "?client_id=9292106e6bff609d98bd0df4de1ede06"
+//                //+ "&client_secret=GcveX0t6jBVJV3TT7XOxrFAc13inJUYf"
+//                + "&redirect_url=http://localhost:8080/api/kakaoapi/login/oauth_kakao"
+//                + "&response_type=code";
+//
+//        return reqUrl;
+//    }
+
+    @GetMapping("login/oauth_kakao")
+    public ResponseEntity<?> oauthKakao(@RequestParam(value = "code", required = false)String code) throws IOException {
+        System.out.println("#########" + code);
+        String access_Token = kakaoAPIService.getKakaoAccessToken(code);
+
+        return new ResponseEntity<>(access_Token, HttpStatus.OK);
+    }
+
+}
