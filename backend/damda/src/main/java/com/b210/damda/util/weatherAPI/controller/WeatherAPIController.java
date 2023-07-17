b4af1aec76f7335ba77ac4e22f7e486af95e881d
신창학 <csg1353@naver.com>
2023-07-17 17:34:30 +0900

    feat : propertes.yml 변경 및 WeatherAPI 추가

--- backend/damda/src/main/java/com/b210/damda/util/weatherAPI/controller/WeatherAPIController.java
+++ backend/damda/src/main/java/com/b210/damda/util/weatherAPI/controller/WeatherAPIController.java
@@ -1,10 +1,15 @@
 package com.b210.damda.util.weatherAPI.controller;
 
+import com.b210.damda.util.weatherAPI.service.WeatherAPIService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 @CrossOrigin("*")
@@ -14,4 +19,10 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class WeatherAPIController {
 
+    private final WeatherAPIService weatherAPIService;
+    @GetMapping("/")
+    public void test() throws Exception {
+        List<Float> temp = new ArrayList<>();
+        weatherAPIService.getWeatherInfos(temp);
+    }
 }
