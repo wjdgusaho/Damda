2a8ca0bf982aff157c94b9dc1a528be22fe54958
박기현 <qkrrlgus114@naver.com>
2023-07-17 14:31:36 +0900

    init : 스프링 프로젝트 생성

---
+++ backend/damda/src/main/java/com/b210/damda/DamdaApplication.java
@@ -0,0 +1,13 @@
+package com.b210.damda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DamdaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DamdaApplication.class, args);
+	}
+
+}
