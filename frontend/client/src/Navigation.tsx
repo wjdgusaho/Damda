df25eb67cf7a43a949a34a39747710c5a9195d68
ChaYoungBum <how8160@naver.com>
2023-07-17 13:51:27 +0900

    feat : react-router기능 추가하여 각각 컴포넌트 url지정 및 페이지별 컴포넌트 구성. chore : axios, styled-component 패키지 추가.

---
+++ frontend/client/src/Navigation.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import styled from 'styled-components';
+
+const Navbar = styled.nav`
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: wrap;
+    width: 120px;
+`;
+
+const Navigation = function () {
+    return (
+        <Navbar>
+            <Link to="/">메인페이지</Link>
+            <Link to="/user">마이페이지</Link>
+            <Link to="/shop">상점페이지</Link>
+            <Link to="/timecapsule">타임캡슐페이지</Link>
+            <Link to="/card">카드페이지</Link>
+            <Link to="/friend">친구페이지</Link>
+        </Navbar>
+    )
+}
+
+export default Navigation
---
+++ frontend/client/src/components/MainPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MainPage = function () {
+    return (
+        <div>
+            Main
+        </div>
+    )
+}
+
+export default MainPage
