2b7d33f99fd27cbbbaceae1cdd7d5a3c5bd5711e
신창학 <csg1353@naver.com>
2023-07-17 15:56:30 +0900

    feat : gradle 추가 (jwt, webclient)

---
+++ backend/damda/src/main/java/com/b210/damda/util/weatherAPI/service/WeatherAPIServiceImpl.java
@@ -0,0 +1,17 @@
+package com.b210.damda.util.weatherAPI.service;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class WeatherAPIServiceImpl implements WeatherAPIService {
+
+//    private final WebClient webClient;
+//
+//    public WeatherNowAPIService() {
+//        this.webClient = WebClient.create();
+//    }
+
+}
