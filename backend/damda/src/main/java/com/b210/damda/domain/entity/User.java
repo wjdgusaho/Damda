2bc25acb5996c9c89c30756049f65b8b53da8388
qkrrlgus114 <qkrrlgus114@naver.com>
2023-07-17 23:37:50 +0900

    feat : 토큰 인증, 토큰 생성, 시큐리티 적용, 유저 업데이트

--- backend/damda/src/main/java/com/b210/damda/domain/entity/User.java
+++ backend/damda/src/main/java/com/b210/damda/domain/entity/User.java
@@ -1,5 +1,6 @@
 package com.b210.damda.domain.entity;
 
+import com.b210.damda.domain.dto.UserUpdateDTO;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.ToString;
@@ -8,6 +9,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
@@ -20,16 +22,28 @@ public class User {
     private String email;
     private String password;
     private String nickname;
-    private String createDate;
+    private LocalDateTime createDate;
+    private LocalDateTime updateDate;
 
     public User() {
     }
 
-    public User(Long id, String email, String password, String nickname, String createDate) {
+    public User(Long id, String email, String password, String nickname, LocalDateTime createDate, LocalDateTime updateDate) {
         this.id = id;
         this.email = email;
         this.password = password;
         this.nickname = nickname;
         this.createDate = createDate;
+        this.updateDate = updateDate;
+    }
+
+    public void updatePassword(String newPassword) {
+        this.updateDate = LocalDateTime.now();
+        this.password = newPassword;
+    }
+
+    public void updateNickname(String newNickname) {
+        this.updateDate = LocalDateTime.now();
+        this.nickname = newNickname;
     }
 }
