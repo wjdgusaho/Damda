f4900bf56ccc216336ec525f7839d8a0aedc63f3
박기현 <qkrrlgus114@naver.com>
2023-07-17 14:18:02 +0900

    remove : 기존에 있던 스프링 깡통 삭제합니다.

--- backend/ssafy/src/main/java/test/ssafy/domain/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package test.ssafy.domain.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import test.ssafy.domain.entity.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
