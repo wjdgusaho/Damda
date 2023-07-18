9d7b0ecfc149a723825c1f273e94c662d03873d6
ChaYoungBum <how8160@naver.com>
2023-07-18 11:10:58 +0900

    refactor : 비밀번호 확인 로직 구현 중

--- frontend/client/src/urls.tsx
+++ frontend/client/src/urls.tsx
@@ -1,4 +1,4 @@
-export const serverUrl = "http://localhost:8080"
+export const serverUrl = "http://localhost:8080/api/"
 export const reqUrl = "https://kauth.kakao.com/oauth/authorize"
                     + "?client_id=9292106e6bff609d98bd0df4de1ede06"
                     + "&client_secret=GcveX0t6jBVJV3TT7XOxrFAc13inJUYf"   
