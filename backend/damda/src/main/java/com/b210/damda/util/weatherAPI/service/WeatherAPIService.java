8bb48d925d38fdfac5e3352083f27f60c7598c3f
신창학 <csg1353@naver.com>
2023-07-18 10:19:44 +0900

    feat : 현재시간 -1시간으로 적용하도록 변경, 입력 파라미터 WeatherDTO 추가

--- backend/damda/src/main/java/com/b210/damda/util/weatherAPI/service/WeatherAPIService.java
+++ backend/damda/src/main/java/com/b210/damda/util/weatherAPI/service/WeatherAPIService.java
@@ -1,11 +1,8 @@
 package com.b210.damda.util.weatherAPI.service;
 
 import com.b210.damda.domain.dto.WeatherDTO;
-import org.springframework.web.bind.annotation.RequestBody;
 import reactor.core.publisher.Mono;
 
-import java.util.List;
-
 public interface WeatherAPIService {
-    Mono<String> getNowWeatherInfos(double lat, double lan) throws Exception;
+    Mono<String> getNowWeatherInfos(WeatherDTO weatherDTO) throws Exception;
 }
--- backend/damda/src/main/java/com/b210/damda/util/weatherAPI/service/WeatherAPIServiceImpl.java
+++ backend/damda/src/main/java/com/b210/damda/util/weatherAPI/service/WeatherAPIServiceImpl.java
@@ -5,7 +5,6 @@ import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.util.UriComponentsBuilder;
 import reactor.core.publisher.Mono;
@@ -13,7 +12,6 @@ import reactor.core.publisher.Mono;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
-import java.util.List;
 
 /**
  * 기상청 데이터 :
@@ -33,21 +31,35 @@ public class WeatherAPIServiceImpl implements WeatherAPIService {
     }
 
     @Override
-    public Mono<String> getNowWeatherInfos(double lat, double lan) throws Exception {
-        //해당 class가 static class이기 때문에 인스턴스 객체 생성할 필요가 없음
-        LatXLngY latXLngY = ConvertGRID_GPS.converting(true, 37.309894444444, 127.644311111111);
+    public Mono<String> getNowWeatherInfos(WeatherDTO weatherDTO) throws Exception {
+        //URL Encoding Issue로 인한 문자열 대체
+        String EncodeServiceKey = serviceKey.replace("+", "%2B");
 
         String baseUrl = "http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtNcst";
         String pageNo = "1";
         String numOfRows = "10";
         String dataType = "JSON";
-        String baseDate = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMdd"));
-        String baseTime = LocalTime.now().format(DateTimeFormatter.ofPattern("HHmm"));
+
+        /*
+        Static Class를 통한 위경도 -> GRID 변환.
+        현재 기상청 API는 위경도값을 입력 파라미터로 받지 않음.
+        따라서 GRID 격자 변환값인 nx, ny로 치환하였음.
+         */
+        LatXLngY latXLngY = ConvertGRID_GPS.converting(weatherDTO.isMode(), weatherDTO.getLat(), weatherDTO.getLan());
         String nx = Integer.toString((int)latXLngY.x);
         String ny = Integer.toString((int)latXLngY.y);
 
-        //URL Encoding Issue로 인한 문자열 대체
-        String EncodeServiceKey = serviceKey.replace("+", "%2B");
+        //현재 시간의 1시간 전 날짜를 API 입력 포맷으로 변환
+        String baseTime = LocalTime
+                .now()
+                .minusHours(1)
+                .format(DateTimeFormatter.ofPattern("HHmm"));
+
+        LocalDateTime tempBaseDate = LocalDateTime.now();
+        if(baseTime.equals("2300")) {
+            tempBaseDate = tempBaseDate.minusDays(1);
+        }
+        String baseDate = tempBaseDate.format(DateTimeFormatter.ofPattern("yyyyMMdd"));
 
         //요소들 조합하여 최종 요청 URL 작성
         String mainUrl = UriComponentsBuilder.fromHttpUrl(baseUrl)
