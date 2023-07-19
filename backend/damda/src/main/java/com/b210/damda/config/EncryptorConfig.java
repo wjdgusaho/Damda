03c71f08ced703d95481788e37f7e254cc0aece6
박기현 <qkrrlgus114@naver.com>
2023-07-19 13:32:38 +0900

    feat : 비밀번호 암호화 추가

---
+++ backend/damda/src/main/java/com/b210/damda/config/EncryptorConfig.java
@@ -0,0 +1,13 @@
+package com.b210.damda.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+public class EncryptorConfig {
+    @Bean
+    public BCryptPasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
