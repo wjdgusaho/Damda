ff78266e350128ec318c7b0aeb42d288e05bd862
ChaYoungBum <how8160@naver.com>
2023-07-20 17:51:26 +0900

    refactor : 랜딩페이지 애니메이션 수정

--- frontend/client/src/components/LandingPage.tsx
+++ frontend/client/src/components/LandingPage.tsx
@@ -1,14 +1,27 @@
-import React from "react";
-import { styled } from "styled-components";
+import React from 'react'
+import { styled } from 'styled-components'
 
 const Box = styled.div`
     display: flex;
     justify-content: center;
-    width: 300px;   
+    width: 300px;
     flex-wrap: wrap;
     margin-left: auto;
     margin-right: auto;
     margin-top: 150px;
+    animation: 3s ease-in-out 0s 1 alternate show-image;
+
+    @keyframes show-image {
+        from {
+            opacity: 0;
+            transform: translateY(-50px);
+            transform: rotate3d(0, 10, 0, 45deg);
+        }
+        to {
+            opacity: 1;
+            transform: rotate3d(0, 0, 0, 45deg);
+        }
+    }
 `
 
 const Person = styled.img`
@@ -31,13 +44,12 @@ const Shadow = styled.div`
 `
 
 export const LandingPage = function () {
-
     return (
         <Box>
             <img src="logo.png" alt="logo" />
-            <p style={{fontFamily: 'PyeongChangPeace', opacity: '50%'}}>우리의 추억을 타임캡슐에 담아요어쩌구</p>
-            <img src="assets/UFO.png" alt="UFO" width="250px" height="250px"/>
-            <Person src="assets/Astronaut-1.png" alt="Astronaut" width="130px" height="130px"/>
+            <p style={{ fontFamily: 'PyeongChangPeace', opacity: '50%' }}>우리의 추억을 타임캡슐에 담아요어쩌구</p>
+            <img src="assets/UFO.png" alt="UFO" width="250px" height="250px" />
+            <Person src="assets/Astronaut-1.png" alt="Astronaut" width="130px" height="130px" />
             <Shadow />
         </Box>
     )
