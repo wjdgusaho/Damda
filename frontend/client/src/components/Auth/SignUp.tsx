6cf892518391e802caf3c1cd601420b1b0ffbe8d
ChaYoungBum <how8160@naver.com>
2023-07-24 12:47:04 +0900

    feat : 회원가입 폼 구성(#177), refactor : urls.tsx 파일의 서버주소 변경

--- frontend/client/src/components/Auth/SignUp.tsx
+++ frontend/client/src/components/Auth/SignUp.tsx
@@ -1,7 +1,7 @@
-import React, {useState, useEffect} from 'react'
-import { styled } from "styled-components";
+import React, { useState, useEffect } from 'react'
+import { styled } from 'styled-components'
 import axios from 'axios'
-import {serverUrl} from '../../urls'
+import { serverUrl } from '../../urls'
 
 const Form = styled.form`
     display: flex;
@@ -13,95 +13,96 @@ const Form = styled.form`
 
 export const SignUp = function () {
     const [userdata, setUserdata] = useState({
-        'email' : '',
-        'password' : '',
-        'passwordCheck' : '',
-        'nickname' : '',
-        'profilePicture' : undefined,
-        'recommendNickname' : ''
+        email: '',
+        password: '',
+        passwordCheck: '',
+        nickname: '',
+        profilePicture: undefined,
+        recommendNickname: '',
     })
     const [passwordMatch, setPasswordMatch] = useState('')
 
-    function handleChange (event:React.FormEvent<HTMLInputElement>) {
+    function handleChange(event: React.FormEvent<HTMLInputElement>) {
         setUserdata({
             ...userdata,
-            [event.currentTarget.name] : event.currentTarget.value,
+            [event.currentTarget.name]: event.currentTarget.value,
         })
     }
 
     useEffect(() => {
-        if(userdata.password && userdata.passwordCheck){
-            if(userdata.password === userdata.passwordCheck){
+        if (userdata.password && userdata.passwordCheck) {
+            if (userdata.password === userdata.passwordCheck) {
                 setPasswordMatch('일치')
-            }
-            else {
+            } else {
                 setPasswordMatch('불일치')
             }
-        }
-        else {
+        } else {
             setPasswordMatch('')
         }
     }, [userdata.password, userdata.passwordCheck])
 
-    function checkEmailOverlap (event:React.MouseEvent<HTMLButtonElement>) {
-        // 아직 안만듬.
+    function checkEmailOverlap(event: React.MouseEvent<HTMLButtonElement>) {
         axios({
-            method: "GET",
-            url : serverUrl + "몰라/",
-            data : {'email' : userdata.email}
+            method: 'GET',
+            url: serverUrl + 'regist',
+            data: { email: userdata.email },
         })
-        .then(response => {
-            console.log(response)
-        })
-        .catch(error => console.error(error))
+            .then((response) => {
+                console.log(response)
+            })
+            .catch((error) => console.error(error))
     }
 
-    function handleSubmit (event:React.FormEvent<HTMLFormElement>){
+    function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
         event.preventDefault()
         axios({
-            method: "POST",
-            url: serverUrl + "regist/",
+            method: 'POST',
+            url: serverUrl + 'regist/',
             headers: {
-                'Content-Type': 'multipart/form-data'
+                'Content-Type': 'multipart/form-data',
             },
             data: {
-                'email': userdata.email,
-                'password': userdata.password,
-                'nickname': userdata.nickname
-            }
+                email: userdata.email,
+                password: userdata.password,
+                nickname: userdata.nickname,
+            },
         })
-        .then(response => {
-            console.log(response)
-            
-        })
-        .catch(error => console.error(error))
+            .then((response) => {
+                console.log(response)
+            })
+            .catch((error) => console.error(error))
     }
 
     return (
         <div>
-          <Form onSubmit={handleSubmit}>
-            <label htmlFor="email">Email</label>
-            <input name='email' type="email" value={userdata.email} onChange={handleChange}/>
-            <button onClick={checkEmailOverlap}>중복체크</button>
+            <Form onSubmit={handleSubmit}>
+                <label htmlFor="email">Email</label>
+                <input name="email" type="email" value={userdata.email} onChange={handleChange} />
+                <button onClick={checkEmailOverlap}>중복체크</button>
 
-            <label htmlFor="password">Password</label>
-            <input name='password' type="password" value={userdata.password} onChange={handleChange}/>
+                <label htmlFor="password">Password</label>
+                <input name="password" type="password" value={userdata.password} onChange={handleChange} />
 
-            <label htmlFor="passwordCheck">Password Check</label>
-            <input name='passwordCheck' type="password" value={userdata.passwordCheck} onChange={handleChange}/>
-            <span>{passwordMatch}</span>
+                <label htmlFor="passwordCheck">Password Check</label>
+                <input name="passwordCheck" type="password" value={userdata.passwordCheck} onChange={handleChange} />
+                <span>{passwordMatch}</span>
 
-            <label htmlFor="nickname">Nickname</label>
-            <input name='nickname' type="text" value={userdata.nickname} onChange={handleChange}/>
+                <label htmlFor="nickname">Nickname</label>
+                <input name="nickname" type="text" value={userdata.nickname} onChange={handleChange} />
 
-            <label htmlFor="profilePicture">Profile Picture</label>
-            <input name="profilePicture" type="file" value={userdata.profilePicture} onChange={handleChange}/>
+                <label htmlFor="profilePicture">Profile Picture</label>
+                <input name="profilePicture" type="file" value={userdata.profilePicture} onChange={handleChange} />
 
-            <label htmlFor="recommendNickname">Recommend Nickname</label>
-            <input name="recommendNickname" type="text" value={userdata.recommendNickname} onChange={handleChange}/>
+                <label htmlFor="recommendNickname">Recommend Nickname</label>
+                <input
+                    name="recommendNickname"
+                    type="text"
+                    value={userdata.recommendNickname}
+                    onChange={handleChange}
+                />
 
-            <button>제출</button>
-          </Form>
+                <button>제출</button>
+            </Form>
         </div>
     )
 }
