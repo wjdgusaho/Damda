bb1d61d4752ccefded937b283a5138a7694748e1
ChaYoungBum <how8160@naver.com>
2023-07-24 16:47:18 +0900

    feat : 뒤로가는 화살표 버튼 추가

--- frontend/client/src/components/Auth/SignUp.tsx
+++ frontend/client/src/components/Auth/SignUp.tsx
@@ -105,8 +105,24 @@ export const SignUp = function () {
     }
 
     return (
-        <div className="grid grid-cols-1 w-48 mx-auto">
-            <Link to={'/login'}>뒤로가기</Link>
+        <div className="grid grid-cols-1 w-48 mx-auto mt-5">
+            <Link to={'/login'} style={{ fontSize: '30px' }}>
+                <svg
+                    className="w-6 h-6 text-black-800 dark:text-white"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 8 14"
+                >
+                    <path
+                        stroke="currentColor"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M7 1 1.3 6.326a.91.91 0 0 0 0 1.348L7 13"
+                    />
+                </svg>
+            </Link>
             <Form className="grid grid-cols-1 w-full mx-auto" onSubmit={handleSubmit}>
                 <div style={{ margin: 'auto' }}>
                     <img
