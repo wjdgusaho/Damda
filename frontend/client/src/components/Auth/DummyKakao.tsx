0371c24ffb5c263e2174aec4411759ba14dd1ca3
ChaYoungBum <how8160@naver.com>
2023-07-25 16:48:24 +0900

    feat : 카카오 로그인 url에서 쿼리값 추출 구현(#266)

--- frontend/client/src/components/Auth/DummyKakao.tsx
+++ frontend/client/src/components/Auth/DummyKakao.tsx
@@ -1,31 +1,35 @@
 import axios from 'axios'
-import React, { useEffect } from 'react'
+import React from 'react'
 import { serverUrl } from '../../urls'
 import { setRefreshToken } from '../../store/Cookie'
 import { useDispatch } from 'react-redux'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, useLocation } from 'react-router-dom'
 import { SET_TOKEN } from '../../store/Auth'
 
 export const DummyKakao = () => {
     const navigate = useNavigate()
     const dispatch = useDispatch()
 
-    useEffect(() => {
-        axios({
-            method: 'GET',
-            url: serverUrl + '/api/kakao/login',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            data: {},
-        })
-            .then((response) => {
-                setRefreshToken(response.data.refreshToken)
-                dispatch(SET_TOKEN(response.data.accessToken))
-                navigate('/main')
-            })
-            .catch((err) => console.error(err))
-    }, [])
+    let getParameter = (key: string) => {
+        return new URLSearchParams(window.location.search).get(key)
+    }
+    let code = getParameter('code')
+    console.log(code)
+
+    // axios({
+    //     method: 'GET',
+    //     url: serverUrl + 'api/kakao/login',
+    //     headers: {
+    //         'Content-Type': 'application/json',
+    //     },
+    //     data: {},
+    // })
+    //     .then((response) => {
+    //         setRefreshToken(response.data.refreshToken)
+    //         dispatch(SET_TOKEN(response.data.accessToken))
+    //         // navigate('/main')
+    //     })
+    //     .catch((err) => console.error(err))
 
     return <div></div>
 }
