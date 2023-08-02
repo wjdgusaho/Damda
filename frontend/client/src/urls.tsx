c7b18713c992519973d961855bc856f099fe5486
ChaYoungBum <how8160@naver.com>
2023-08-02 12:18:02 +0900

    fix : https 형식으로 url 수정

--- frontend/client/src/urls.tsx
+++ frontend/client/src/urls.tsx
@@ -1,5 +1,6 @@
 // export const serverUrl = "http://localhost:8080/"
-export const serverUrl = "http://i9b210.p.ssafy.io:1104/"
+export const serverUrl = "https://i9b210.p.ssafy.io:1104/"
 
 // export const reqUrl = "http://localhost:8080/oauth2/authorization/kakao"
-export const reqUrl = "http://i9b210.p.ssafy.io:1104/oauth2/authorization/kakao"
+export const reqUrl =
+  "https://i9b210.p.ssafy.io:1104/oauth2/authorization/kakao"
