201e5207a96cb1d97e0e37b812a3d9b7f30be679
정현모 <wjdgusaho@naver.com>
2023-08-02 14:24:24 +0900

    feat : 타임캡슐 상세 페이지 기능 (#402)

---
+++ backend/damda/src/main/java/com/b210/damda/domain/dto/Timecapsule/detailchild/DetailCriteria.java
@@ -0,0 +1,21 @@
+package com.b210.damda.domain.dto.Timecapsule.detailchild;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.security.Timestamp;
+@Data
+@Getter @Setter
+@Builder
+public class DetailCriteria {
+
+    private String criteriaType;
+    private Timestamp startTime;
+    private Timestamp endTime;
+    private String location;
+    private String weatherStatus;
+    private String timeKr;
+
+}
