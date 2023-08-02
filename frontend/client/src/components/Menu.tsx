58eab858eed877d4188c0f338a96826976951023
권소정 <flfk33@naver.com>
2023-08-02 21:01:51 +0900

    feat: urls.tsx 수정

--- frontend/client/src/components/Menu.tsx
+++ frontend/client/src/components/Menu.tsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react"
 import "../index.css"
-import tw from "tailwind-styled-components"
 import { styled } from "styled-components"
 import { useNavigate } from "react-router"
 // import { Store } from "../store/Store"
