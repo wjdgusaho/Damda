0b75175532e807fad75561ec7c0620cbcdaf110e
신창학 <csg1353@naver.com>
2023-08-02 10:59:56 +0900

    feat : 현재 위경도값에 가장 가까운 지역 정보 리턴

--- backend/damda/src/main/java/com/b210/damda/domain/dto/WeatherDTO.java
+++ backend/damda/src/main/java/com/b210/damda/domain/dto/WeatherDTO.java
@@ -1,16 +1,27 @@
 package com.b210.damda.domain.dto;
 
-import lombok.Data;
+import lombok.*;
 
 /**
  * mode : true일 경우 위경도->GRID변환
  * false일 경우 반대
  */
 @Data
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class WeatherDTO {
     boolean mode;
     double lat;
     double lan;
     int nx;
     int ny;
+    String content;
+
+    @Builder
+    public WeatherDTO(double lat, double lan, int nx, int ny, String content) {
+        this.lan = lan;
+        this.lat = lat;
+        this.nx = nx;
+        this.ny = ny;
+        this.content = content;
+    }
 }
--- backend/damda/src/main/java/com/b210/damda/util/weatherAPI/service/WeatherLocationServiceImpl.java
+++ backend/damda/src/main/java/com/b210/damda/util/weatherAPI/service/WeatherLocationServiceImpl.java
@@ -17,16 +17,40 @@ public class WeatherLocationServiceImpl implements WeatherLocationService {
     private final WeatherLocationRepository weatherLocationRepository;
     @Override
     public WeatherDTO getNowLocation(WeatherDTO weatherDTO) throws Exception {
-        // WeatherDTO에서 lan과 lat 값을 가져옴
-        double lan = weatherDTO.getLan();
-        double lat = weatherDTO.getLat();
-        int nx = weatherDTO.getNx();
-        int ny = weatherDTO.getNy();
 
-        List<WeatherLocation> weatherLocations = weatherLocationRepository.findByNxAndNy(nx, ny);
+        //DTO에서 위경도값과 변환 모드 넣고 변환하는 클래스에 돌린 뒤, LatXLngY 인스턴스에 값 대입
+        double lat = weatherDTO.getLat();
+        double lan = weatherDTO.getLan();
+        LatXLngY latXLngY = ConvertGRID_GPS.converting(weatherDTO.isMode(), lat, lan);
+
+        //격자값 변환한 nx,ny와 동일한 값 찾고, 이 중 위경도와 가장 유사한 값을 찾음
+        List<WeatherLocation> weatherLocations = weatherLocationRepository.findByNxAndNy((int)latXLngY.x, (int)latXLngY.y);
+
         for (int i = 0; i < weatherLocations.size(); i++) {
+            WeatherLocation now = weatherLocations.get(i);
+            //현재좌표와 같은 격자값들을 가진 장소들의 좌표값 차이를 비교
+            double nowVal = Math.abs(lat - now.getLat()) + Math.abs(lan - now.getLan());
+            if(DistVal.maxVal >= nowVal) {
+                DistVal.maxVal = nowVal;
+                DistVal.result = now;
+            }
             log.info("{}, {}", weatherLocations.get(i).getLocalMedium(), weatherLocations.get(i).getLocalSmall());
         }
-        return new WeatherDTO();
+
+        //최종 가장 근접한 지역의 결과값을 WeatherDTO 형태로 반출
+        WeatherDTO result = WeatherDTO.builder()
+                .nx(DistVal.result.getNx())
+                .ny(DistVal.result.getNy())
+                .lat(DistVal.result.getLat())
+                .lan(DistVal.result.getLan())
+                .content(DistVal.result.getLocalBig() + " " + DistVal.result.getLocalMedium() + " " + DistVal.result.getLocalSmall())
+                .build();
+
+        return result;
     }
+}
+
+class DistVal {
+    static double maxVal = Double.MAX_VALUE;
+    static WeatherLocation result = new WeatherLocation();
 }
