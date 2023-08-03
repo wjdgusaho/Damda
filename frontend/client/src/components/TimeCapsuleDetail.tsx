33079e4cb10c6a38944dd06119746b6c9081f4bb
ChaYoungBum <how8160@naver.com>
2023-08-03 09:29:11 +0900

    feat : params로 타임캡슐 상세페이지에서 id값 정보전달

--- frontend/client/src/components/TimeCapsuleDetail.tsx
+++ frontend/client/src/components/TimeCapsuleDetail.tsx
@@ -2,7 +2,7 @@ import React, { useState } from "react"
 import "../index.css"
 import tw from "tailwind-styled-components"
 import { styled } from "styled-components"
-import { useNavigate } from "react-router"
+import { useNavigate, useParams } from "react-router"
 import { SubHeader } from "./inc/SubHeader"
 import axios from "axios"
 import { serverUrl } from "../urls"
@@ -10,7 +10,9 @@ import { useSelector } from "react-redux"
 import { RootState } from "../store/Store"
 
 const TimeCapsuleDetail = function () {
-  return <div>타임캡슐 상세</div>
+  const { capsuleId } = useParams()
+
+  return <div>타임캡슐 상세{capsuleId}</div>
 }
 
 export default TimeCapsuleDetail
