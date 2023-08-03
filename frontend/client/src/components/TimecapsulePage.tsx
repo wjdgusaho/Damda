b76de6d5b27226329750b3a0ed1e0138dec35c0d
Jiyoung0913 <xkagja2006@naver.com>
2023-08-03 17:20:24 +0900

    fix. 주석 에러 수정

--- frontend/client/src/components/TimecapsulePage.tsx
+++ frontend/client/src/components/TimecapsulePage.tsx
@@ -218,7 +218,7 @@ const TimecapsulePage = function () {
                   </CapsuleTitle>
                 </div>
               </Card>
-            )} */}
+            )}
           </React.Fragment>
         ))}
       </Box>
