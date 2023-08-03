7cc773ee8b7fc553ddf6859de25010660270dfc9
ChaYoungBum <how8160@naver.com>
2023-08-03 11:00:54 +0900

    feat : redux-persist 패키지 추가, timecapsule store export 변경

--- frontend/client/src/store/Store.tsx
+++ frontend/client/src/store/Store.tsx
@@ -2,11 +2,13 @@ import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit"
 import authReducer from "./Auth"
 import thunk from "redux-thunk"
 import theme from "./Theme"
+import Timecapsule from "./Timecapsule"
 
 const Store = configureStore({
   reducer: {
     auth: authReducer,
     theme: theme.reducer,
+    timecapsule: Timecapsule,
   },
   middleware: [...getDefaultMiddleware(), thunk],
 })
