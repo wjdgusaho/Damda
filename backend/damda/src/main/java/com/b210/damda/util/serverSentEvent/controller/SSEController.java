89dc00a9ecb664ff8ce18262de55e34c59316cb8
신창학 <csg1353@naver.com>
2023-08-09 13:40:40 +0900

    test : 전부 삭제

--- backend/damda/src/main/java/com/b210/damda/util/serverSentEvent/controller/SSEController.java
+++ backend/damda/src/main/java/com/b210/damda/util/serverSentEvent/controller/SSEController.java
@@ -30,7 +30,7 @@ public class SSEController {
 
     //최초 접속 시 로그인 이벤트. 이를 통해 스트림 파이프라인 구축 가능
     //MediaType 명시를 통해 엔드포인트가 text/event-stream을 반환하도록 강제함.
-    @GetMapping(value = "/sse/login")
+    @GetMapping(value = "/sse/login", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public Flux<ServerSentEvent<String>> login() {
         log.info("로그인 개방");
         //1. 확인하지 못했던 친구 상태 알림 로직
@@ -41,31 +41,21 @@ public class SSEController {
     }
 
     //로그아웃 시 스트림 제거
-    @GetMapping(value = "/sse/logout")
+    @GetMapping(value = "/sse/logout", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public void logout() {
         eventStreamService.disconnectStream();
     }
 
-    //heartbeat 응답 스트림
-    @GetMapping(value = "/sse/check")
+    //heartbeat 응답 스트림 받기
+    @GetMapping(value = "/sse/check", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public void checkConnection() {
         eventStreamService.checkConnection();
     }
     //테스트용
 
-    @GetMapping(value = "/sse/test/request")
+    @GetMapping(value = "/sse/test/request", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public void test(@RequestParam long no) {
         friendEventService.friendRequestEvent(no);
-    }
-
-    @GetMapping(value = "/sse/test/accept")
-    public void test2(@RequestParam long no) {
-        friendEventService.friendAcceptEvent(no);
-    }
-
-    @GetMapping(value = "/sse/test/reject")
-    public void test3(@RequestParam long no) {
-        friendEventService.friendDenyEvent(no);
     }
 
 }
