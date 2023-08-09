5905060e1e305ae610db0a4318fdafb5f5ed1f27
신창학 <csg1353@naver.com>
2023-08-09 16:35:33 +0900

    Revert "Merge branch 'feature/backend/sseV5' into 'develop'"
    This reverts merge request !625

--- backend/damda/src/main/java/com/b210/damda/util/schedular/service/ScheduledService.java
+++ backend/damda/src/main/java/com/b210/damda/util/schedular/service/ScheduledService.java
@@ -1,15 +1,14 @@
-package com.b210.damda.util.schedular;
+package com.b210.damda.util.schedular.service;
 
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
-@Component
+@Service
 @Slf4j
-public class ScheduledComponent {
-    @Scheduled(fixedRate = 1000) // 11초마다 실행
+public class ScheduledService {
+    @Scheduled(fixedRate = 10000) // 10초마다 실행
     public void scheduledTask() {
         log.info("스케줄링된 작업 실행: {}", System.currentTimeMillis());
     }
