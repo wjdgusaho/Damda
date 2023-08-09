7185135077d84df4a9484bcedff75461803e2bee
ChaYoungBum <how8160@naver.com>
2023-08-09 17:03:50 +0900

    feat : 콘솔 관련 테스트

--- frontend/client/src/components/TimecapsuleOpen.tsx
+++ frontend/client/src/components/TimecapsuleOpen.tsx
@@ -5,8 +5,10 @@ export const TimecapsuleOpen = function () {
 
   let lastBeta: number | null = null
   let lastGamma: number | null = null
+
   function handleOrientation(event: DeviceOrientationEvent) {
     const { alpha, beta, gamma } = event
+    console.log("alpha : ", alpha, "beta : ", beta, "gamma : ", gamma)
 
     if (lastBeta === null || lastGamma === null) {
       lastBeta = beta
@@ -18,7 +20,7 @@ export const TimecapsuleOpen = function () {
       const betaDiff = Math.abs(beta - lastBeta)
       const gammaDiff = Math.abs(gamma - lastGamma)
 
-      if (betaDiff > 20 || gammaDiff > 20) {
+      if (betaDiff > 10 || gammaDiff > 10) {
         // 휴대전화가 흔들렸을 때 실행할 코드를 여기에 작성합니다.
         console.log("휴대전화가 흔들렸습니다!")
       }
@@ -31,7 +33,7 @@ export const TimecapsuleOpen = function () {
     <div className="text-center">
       <p>타임캡슐 열리는 모습 보이기</p>
       <p className="text-black bg-white">
-        {lastBeta}, {lastGamma}
+        {lastBeta ? lastBeta : 0}, {lastGamma ? lastGamma : 0}
       </p>
     </div>
   )
