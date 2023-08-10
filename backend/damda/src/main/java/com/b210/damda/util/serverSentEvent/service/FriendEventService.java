619ceaf01dc5dc8feee3e92c8dbcd69c5b38b777
신창학 <csg1353@naver.com>
2023-08-10 10:59:01 +0900

    fix : 필요없는 주석 제거

--- backend/damda/src/main/java/com/b210/damda/util/serverSentEvent/service/FriendEventService.java
+++ backend/damda/src/main/java/com/b210/damda/util/serverSentEvent/service/FriendEventService.java
@@ -50,33 +50,9 @@ public class FriendEventService {
     }
 
 
-//    //친구 요청 이벤트 : 나(userNo) -> 친구 요청하는 사용자(frinedNo)
-//    public void friendRequestEvent(long friendNo) {
-//        Long userNo = addOnEventService.getUserNo();
-//        log.info("가져온 유저 값 : ", userNo);
-//
-//        //eventBuilder로 이벤트 생성
-//        ServerSentEvent<JsonNode> event = addOnEventService.buildServerSentEvent("custom-event", new ServerSentEventDTO(userNo.toString(), "로부터 친구 요청이 도착했습니다. ", addOnEventService.getNowTime()));
-//        eventStreamService.sendEvent(friendNo, event);
-//    }
-//
-//    //친구 요청 수락 이벤트(내가 상대방의 친구 요청을 승낙함)
-//    public void friendAcceptEvent(long fromNo) {
-//        Long userNo = addOnEventService.getUserNo();
-//        ServerSentEvent<JsonNode> event = addOnEventService.buildServerSentEvent("custom-event", new ServerSentEventDTO(userNo.toString(), "님이 친구 요청을 승낙했습니다!", addOnEventService.getNowTime()));
-//        eventStreamService.sendEvent(fromNo, event);
-//    }
-//
-//    //친구 요청 거절 이벤트(요청 받은 내가 상대방에게 알림 전송)
-//    public void friendDenyEvent(long fromNo) {
-//        Long userNo = addOnEventService.getUserNo();
-//        ServerSentEvent<JsonNode> event = addOnEventService.buildServerSentEvent("custom-event", new ServerSentEventDTO(userNo.toString(), "님이 친구 요청을 거절했습니다...", addOnEventService.getNowTime()));
-//        eventStreamService.sendEvent(fromNo, event);
-//    }
-//
-//    //첫 로그인 시 밀렸던 모든 친구들을 조회하기
-//    public void checkAllFriendEvent() {
-//        log.info("부재중 모든 친구 체크 요청 로직 실행");
-//    }
+    //첫 로그인 시 밀렸던 모든 친구들을 조회하기
+    public void checkAllFriendEvent() {
+        log.info("부재중 모든 친구 체크 요청 로직 실행");
+    }
 
 }
