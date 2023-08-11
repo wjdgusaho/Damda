f129c62b602e7d286a99f6167cc2ac693a7330db
신창학 <csg1353@naver.com>
2023-08-11 13:41:17 +0900

    test : @Param 추가 및 레포지토리 변경

--- backend/damda/src/main/java/com/b210/damda/domain/friend/repository/FriendRepository.java
+++ backend/damda/src/main/java/com/b210/damda/domain/friend/repository/FriendRepository.java
@@ -46,8 +46,8 @@ public interface FriendRepository extends JpaRepository<UserFriend, Long> {
     List<Long> findUserNoByFriendNoAndStatus(@Param("friendNo") Long friendNo, @Param("status") String status);
 
     //해당 유저의 이름과 이미지 가져오기
-    @Query("SELECT u.nickname, u.profileImage From User u Where u.userNo = :userNo")
-    UserNameAndImageDTO getUserNameAndImage(Long userNo);
+    @Query("SELECT new com.b210.damda.domain.dto.serverSentEvent.friend.UserNameAndImageDTO(u.nickname, u.profileImage) From User u Where u.userNo = :userNo")
+    UserNameAndImageDTO getUserNameAndImage(@Param("userNo") Long userNo);
 
     //해당 유저로 요청한 사람들의 정보 가져오기
     @Query("SELECT new com.b210.damda.domain.dto.serverSentEvent.friend.GetRequestToMeDTO(uf.user.nickname, uf.user.userNo, uf.requestDate, uf.user.profileImage) FROM UserFriend uf WHERE uf.friend.userNo = :friendNo and uf.status = 'REQUESTED'")
