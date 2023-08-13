a277360f21613a70a0e92d35364b3295af2e0647
신창학 <csg1353@naver.com>
2023-08-13 23:50:15 +0900

    feature/backend/sseV13

--- backend/damda/src/main/java/com/b210/damda/util/schedular/test.java
+++ backend/damda/src/main/java/com/b210/damda/util/schedular/test.java
@@ -1,15 +1,13 @@
 package com.b210.damda.util.schedular;
 
+import com.b210.damda.domain.timecapsule.repository.TimeCapsuleSEERepository;
 import com.b210.damda.util.serverSentEvent.service.AddOnEventService;
 import com.b210.damda.util.serverSentEvent.service.EventStreamService;
 import com.b210.damda.util.serverSentEvent.service.FriendEventService;
-import com.fasterxml.jackson.databind.JsonNode;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.codec.ServerSentEvent;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-import reactor.core.publisher.FluxSink;
 
 @Component
 @RequiredArgsConstructor
@@ -19,12 +17,12 @@ public class test {
     final FriendEventService friendEventService;
     final EventStreamService eventStreamService;
     final AddOnEventService addOnEventService;
+    final TimeCapsuleSEERepository timeCapsuleSEERepository;
+    @Scheduled(fixedRate = 3000) // 테스터
+    public void test() {
+        log.info("테스터 스케줄러 동작");
+//        friendEventService.checkAllFriendEvent();
+        System.out.println(timeCapsuleSEERepository.getUserNoByInviteCode("MDMRIACIX0"));
+    }
 
-//    @Scheduled(fixedRate = 3000) // 테스터
-//    public void test() {
-//        log.info("테스터 스케줄러 동작");
-////        friendEventService.checkAllFriendEvent();
-//        eventStreamService.sendEvent(35L, addOnEventService.buildServerSentEvent("friend-event", null));
-//    }
-//
 }
