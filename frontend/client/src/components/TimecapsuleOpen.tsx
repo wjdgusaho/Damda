bda459596bf65f795ec8498ddb1a5df93d30a1aa
Jiyoung0913 <xkagja2006@naver.com>
2023-08-13 23:17:29 +0900

    feat. 오픈 화면 흔드는 페이지 클릭 구현

--- frontend/client/src/components/TimecapsuleOpen.tsx
+++ frontend/client/src/components/TimecapsuleOpen.tsx
@@ -1,14 +1,50 @@
-import React, { useEffect } from "react"
+import React, { useEffect, useState } from "react"
 import { useNavigate, useParams } from "react-router-dom"
+import { PieChart } from "react-minimal-pie-chart"
+import { styled } from "styled-components"
+import axios from "axios"
+import { useSelector } from "react-redux"
+import { RootState } from "../store/Store"
+import { serverUrl } from "../urls"
+
+interface CapsuleInfoType {
+  timecapsuleNo: number
+  title: string
+  type: string
+  capsuleIconNo: string
+}
 
 export const TimecapsuleOpen = function () {
   const navigate = useNavigate()
   const { capsuleId } = useParams()
+  const token = useSelector((state: RootState) => state.auth.accessToken)
+  const [capsuleInfo, setCapsuleInfo] = useState<CapsuleInfoType | null>(null)
+  const [shakeCnt, setShakeCnt] = useState(0)
 
   let lastAlpha: number | null = null
   let lastBeta: number | null = null
   let lastGamma: number | null = null
-  let shakeCnt = 30
+
+  useEffect(() => {
+    console.log("token", token)
+    const fetchData = async () => {
+      try {
+        const timecapsuleNo = capsuleId
+        const response = await axios.get(
+          serverUrl + `timecapsule/simpleinfo?timecapsuleNo=${timecapsuleNo}`,
+          {
+            headers: {
+              Authorization: "Bearer " + token,
+            },
+          }
+        )
+        setCapsuleInfo(response.data.data.timecapsuleSimpleInfo)
+      } catch (error) {
+        console.error(error)
+      }
+    }
+    fetchData()
+  }, [])
 
   useEffect(() => {
     window.addEventListener("deviceorientation", handleOrientation)
@@ -17,6 +53,13 @@ export const TimecapsuleOpen = function () {
       window.removeEventListener("deviceorientation", handleOrientation)
     }
   }, [])
+
+  useEffect(() => {
+    console.log("shakeCnt : ", shakeCnt)
+    if (shakeCnt >= 100) {
+      navigate(`/timecapsule/result/${capsuleId}`)
+    }
+  }, [shakeCnt])
 
   function handleOrientation(event: DeviceOrientationEvent) {
     const { alpha, beta, gamma } = event
@@ -36,13 +79,14 @@ export const TimecapsuleOpen = function () {
       if (betaDiff > 70 || gammaDiff > 70 || alphaDiff > 110) {
         // 휴대전화가 흔들렸을 때 실행할 코드를 여기에 작성합니다.
         navigator.vibrate([100, 100, 100, 100])
-        shakeCnt--
-        if (shakeCnt <= 0) {
+        setShakeCnt(shakeCnt + 3)
+        if (shakeCnt >= 100) {
           // 여기에 다음 열린 후에 페이지로 들어가도록 만들어주세요.
           navigate(`/result/${capsuleId}`)
         }
       }
     }
+
     lastAlpha = alpha
     lastBeta = beta
     lastGamma = gamma
@@ -55,6 +99,38 @@ export const TimecapsuleOpen = function () {
   return (
     <div className="text-center">
       {/* 여기에 타임캡슐 이미지 만들고 흔들리는 모션 만들어 주세요. */}
+      <FloatingImage
+        capsulenum={capsuleInfo?.capsuleIconNo ?? "capsule1"}
+        onClick={() => {
+          setShakeCnt(shakeCnt + 3)
+        }}
+      ></FloatingImage>
+      <div>
+        <PieChart
+          className="w-80 absolute left-1/2 -ml-40"
+          data={[{ value: shakeCnt, color: "#ffffff", name: "name1" }]}
+          reveal={shakeCnt}
+          lineWidth={15} //도넛 두께
+          background="#ffffff4b"
+          lengthAngle={360}
+          rounded
+          animate
+        />
+      </div>
     </div>
   )
 }
+
+const FloatingImage = styled.div<{ capsulenum: string }>`
+  position: absolute;
+  top: 50%;
+  margin-top: -125px;
+  left: 50%;
+  margin-left: -125px;
+  background-image: url(/${(props) => props.theme[props.capsulenum]});
+  background-repeat: no-repeat;
+  background-size: cover;
+  width: 250px;
+  height: 250px;
+  z-index: 1;
+`
