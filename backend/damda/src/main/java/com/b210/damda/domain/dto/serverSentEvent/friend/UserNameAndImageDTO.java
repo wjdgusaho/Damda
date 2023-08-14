ecb8b9c707330bf62015483aabb8af48eb926845
신창학 <csg1353@naver.com>
2023-08-14 10:18:41 +0900

    test : 시간

--- backend/damda/src/main/java/com/b210/damda/domain/dto/serverSentEvent/friend/UserNameAndImageDTO.java
+++ backend/damda/src/main/java/com/b210/damda/domain/dto/serverSentEvent/friend/UserNameAndImageDTO.java
@@ -2,10 +2,16 @@ package com.b210.damda.domain.dto.serverSentEvent.friend;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 
-@AllArgsConstructor
-@Data
+
+@Getter
 public class UserNameAndImageDTO {
     String userName;
     String userProfileImage;
+
+    public UserNameAndImageDTO(String userName, String userProfileImage) {
+        this.userName = userName;
+        this.userProfileImage = userProfileImage;
+    }
 }
--- backend/damda/src/main/java/com/b210/damda/util/schedular/test.java
+++ backend/damda/src/main/java/com/b210/damda/util/schedular/test.java
@@ -11,6 +11,9 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Component
 @RequiredArgsConstructor
@@ -25,9 +28,18 @@ public class test {
     @Scheduled(fixedRate = 3000) // 테스터
     public void test() {
         log.info("테스터 스케줄러 동작");
-        LocalDateTime currentTime = LocalDateTime.now();
-        log.info("현재 시간: " + currentTime);
-        //전체 DB 순회하여
+
+        // 서버 시스템 시간 가져오기
+        LocalDateTime serverTime = LocalDateTime.now();
+
+        // 서버 시스템 시간을 Asia/Seoul 기준 시간으로 변환
+        ZonedDateTime seoulTime = serverTime.atZone(ZoneId.of("Asia/Seoul"));
+
+        // 서울 시간을 원하는 형식으로 출력
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        String formattedSeoulTime = seoulTime.format(formatter);
+
+        log.info("현재 서울 시간: " + formattedSeoulTime);
     }
 
 //    //서울시간 00시마다 timecapsule_mapping 테이블에 card_able 컬럼들이 모두 (true 또는 1)로 수정되게 변경
