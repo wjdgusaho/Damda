afffa1161d66030801c7e2a9bf18ce481141a8ff
정현모 <wjdgusaho@naver.com>
2023-08-14 17:32:34 +0900

    feat : 타임캡슐 맵핑 에러 수정

--- backend/damda/src/main/java/com/b210/damda/domain/timecapsule/repository/TimecapsuleMappingRepository.java
+++ backend/damda/src/main/java/com/b210/damda/domain/timecapsule/repository/TimecapsuleMappingRepository.java
@@ -17,21 +17,21 @@ public interface TimecapsuleMappingRepository extends JpaRepository<TimecapsuleM
     List<TimecapsuleMapping> findByUserUserNo(Long userNo);
 
     @Query("SELECT tm FROM TimecapsuleMapping tm WHERE tm.user.userNo = :userNo AND tm.timecapsule.removeDate IS NULL AND tm.deleteDate IS NULL")
-    List<TimecapsuleMapping> findByActiveUserUserNo(Long userNo);
+    List<TimecapsuleMapping> findByActiveUserUserNo(@Param("userNo") Long userNo);
 
     @Query("SELECT t FROM TimecapsuleMapping t " +
             "WHERE t.user.userNo = :userNo " +
             "AND t.timecapsule.removeDate IS NULL " +
             "AND t.deleteDate IS NULL " +
             "AND t.isSave = false")
-    List<TimecapsuleMapping> findWorkTimecapsules(Long userNo);
+    List<TimecapsuleMapping> findWorkTimecapsules(@Param("userNo")Long userNo);
 
     @Query("SELECT t FROM TimecapsuleMapping t " +
             "WHERE t.user.userNo = :userNo " +
             "AND t.timecapsule.removeDate IS NULL " +
             "AND t.deleteDate IS NULL " +
             "AND t.isSave = true")
-    List<TimecapsuleMapping> findSaveTimecapsules(Long userNo);
+    List<TimecapsuleMapping> findSaveTimecapsules(@Param("userNo") Long userNo);
 
 
     @Query("SELECT tm FROM TimecapsuleMapping tm WHERE tm.user.userNo = :userNo AND tm.timecapsule.timecapsuleNo = :timecapsuleNo")
