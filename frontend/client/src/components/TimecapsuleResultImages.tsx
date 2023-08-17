65bbb2e09aee401889493c315449029188a307a3
권소정 <flfk33@naver.com>
2023-08-17 17:07:57 +0900

    feat: 카드 개별 저장 기능 구현

--- frontend/client/src/components/TimecapsuleResultImages.tsx
+++ frontend/client/src/components/TimecapsuleResultImages.tsx
@@ -6,10 +6,12 @@ import { RootState } from "../store/Store"
 import axios from "axios"
 import { useParams } from "react-router-dom"
 import { motion } from "framer-motion"
+import toast, { Toaster } from "react-hot-toast"
 
 interface cardType {
   userNo: string
   imagePath: string
+  timecapsuleCardNo: string
 }
 
 const TimecapsuleResultImages = function () {
@@ -18,15 +20,56 @@ const TimecapsuleResultImages = function () {
   const token = useSelector((state: RootState) => state.auth.accessToken)
   const [cardList, setCardList] = useState<cardType[] | null>(null)
   const { capsuleId } = useParams()
+  const [selectedImgNo, setSelectedImgNo] = useState("")
 
-  const handleImageClick = (imgUrl: string) => {
+  const handleImageClick = (imgUrl: string, imgNo: string) => {
     setSelectedImageUrl(imgUrl)
+    setSelectedImgNo(imgNo)
     setModalOpen(true)
   }
 
   const handleClose = () => {
     setSelectedImageUrl("")
     setModalOpen(false)
+  }
+
+  const getCardImg = async (imgNo: string) => {
+    try {
+      const response = await axios({
+        method: "GET",
+        url:
+          process.env.REACT_APP_SERVER_URL +
+          `s3/download/timecapsule/${capsuleId}/timecapsule-card/${imgNo}`,
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer " + token,
+        },
+        responseType: "blob",
+      })
+      const url = window.URL.createObjectURL(new Blob([response.data]))
+      const link = document.createElement("a")
+      link.href = url
+      link.setAttribute("download", "image.jpg") // 다운로드 받을 때의 파일명을 설정합니다.
+      document.body.appendChild(link)
+      link.click()
+      document.body.removeChild(link) // 링크를 바로 제거합니다.
+      if (response.data.code === 200) {
+      } else if (response.data.code === -6002) {
+        toast("타임캡슐이 존재하지 않습니다.")
+      } else if (response.data.code === -3018) {
+        toast("해당 유저의 타임캡슐이 아닙니다.")
+      } else if (response.data.code === -6008) {
+        toast("삭제된 타임캡슐입니다.")
+      } else if (response.data.code === -3020) {
+        toast("아직 열지 않은 타임캡슐입니다.")
+      } else if (response.data.code === -6005) {
+        toast("작성된 카드가 없습니다.")
+      } else if (response.data.code === -6014) {
+        toast("해당 타임캡슐의 카드가 아닙니다.")
+      }
+    } catch (error) {
+      console.log(error)
+    }
   }
 
   useEffect(() => {
@@ -62,7 +105,9 @@ const TimecapsuleResultImages = function () {
           cardList.map((img) => (
             <Image
               key={img.imagePath}
-              onClick={() => handleImageClick(img.imagePath)}
+              onClick={() =>
+                handleImageClick(img.imagePath, img.timecapsuleCardNo)
+              }
             >
               <img src={img.imagePath} alt="" />
             </Image>
@@ -73,6 +118,11 @@ const TimecapsuleResultImages = function () {
           style={customStyles}
         >
           <div>
+            <DownloadIcon
+              src="../../assets/icons/download.png"
+              alt="download"
+              onClick={() => getCardImg(selectedImgNo)}
+            />
             <img className="w-96" src={selectedImageUrl} alt="Selected" />
           </div>
         </Modal>
@@ -93,6 +143,13 @@ const ImgConainer = styled.div`
 const Image = styled.div`
   margin: 0.5px;
   width: 99px;
+`
+
+const DownloadIcon = styled.img`
+  position: absolute;
+  top: -5px;
+  right: 20px;
+  width: 23px;
 `
 
 const customStyles = {
